9a7630d162579e568eeece49451db18619816da9
연석 <skdustjr2@gmail.com>
2026-04-19 20:57:30 +0900

    use unpooled postgres connection

--- lib/db.ts
+++ lib/db.ts
@@ -5,14 +5,21 @@ declare global {
   var pgPool: Pool | undefined;
 }
 
+const connectionString =
+  process.env.DATABASE_URL_UNPOOLED ||
+  process.env.POSTGRES_URL_NON_POOLING ||
+  process.env.DATABASE_URL ||
+  process.env.POSTGRES_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE URL이 설정되지 않았습니다.");
+}
+
 const pool =
   global.pgPool ??
   new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl:
-      process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false,
+    connectionString,
+    ssl: { rejectUnauthorized: false },
   });
 
 if (process.env.NODE_ENV !== "production") {
